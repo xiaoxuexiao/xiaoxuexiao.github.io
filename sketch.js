215dd9be0e4bd2c78a0d691c123470d20f36cdab
Xiaoxue Xiao <xiaoxuexiao@outlook.com>
2018-01-23 09:49:09 -0500

    Hello to Jiawen

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(1242, 2208);
+  createCanvas(800, 400);
 }
 
 function draw() {
@@ -7,5 +7,5 @@ function draw() {
   fill(20, 204, 50);
   textSize(50);
   fill(20, 50, 90);
-  text('Hello, Echo', 400, 200);
+  text('Hello, Jiawen', 400, 200);
 }
