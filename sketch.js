5b0c2f7bec988490632da908bf9434daab485690
Xiaoxue Xiao <xiaoxuexiao@outlook.com>
2018-01-24 09:19:42 -0500

    change to Jiawen

--- sketch.js
+++ sketch.js
@@ -4,7 +4,10 @@ function setup() {
 
 function draw() {
   background('pink');
+  ellipse(80, 20, 20, 20);
+  ellipse(110, 20, 20, 20);
   fill(20, 204, 50);
+  rect(200, 200, 100, 100);
   textSize(50);
   fill(20, 50, 90);
   text('Hello, Jiawen', 400, 200);
