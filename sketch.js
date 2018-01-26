40ec377377f8a288bb12a5a0af6f5bfd2de822da
Xiaoxue Xiao <xiaoxuexiao@outlook.com>
2018-01-26 14:13:17 -0500

    change color

--- sketch.js
+++ sketch.js
@@ -4,11 +4,12 @@ function setup() {
 
 function draw() {
   background('pink');
+  fill(234, 172, 131);
   ellipse(80, 20, 20, 20);
   ellipse(110, 20, 20, 20);
-  fill(20, 204, 50);
-  rect(200, 200, 100, 100);
+  fill(131, 197, 234);
+  rect(140, 140, 100, 100);
   textSize(50);
-  fill(20, 50, 90);
+  fill(150, 150, 150);
   text('Hello, Jiawen', 400, 200);
 }
