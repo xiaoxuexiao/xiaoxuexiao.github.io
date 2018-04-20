58b222b5bb2250def96783f63b8ea8bf320e88c0
Xiaoxue Xiao <xiaoxuexiao@outlook.com>
2018-04-20 12:07:05 -0400

    7.0 update

--- A7.0/sketch.js
+++ A7.0/sketch.js
@@ -1,6 +1,4 @@
-// This example uses NYT's Top Stories API and visualizes the relative
-// length of story headlines.
-// ---
+
 
 // state data;
 var uber = {};
@@ -9,30 +7,26 @@ var yellowcab = {};
 // state variables
 var uberlat = [];
 var uberlong = [];
+var ubertime = [];
+
 var cablat = [];
 var cablong = [];
 
 var s = 0;
+var f = 0;
 
 
 function preload() {
 
 
-  // Assemble url for API call
-<<<<<<< HEAD
-uber = loadTable('uber-raw-data-20140405-sorted.csv', 'csv', 'header');
-console.log(uber);
+
+uber = loadTable('uber_0401 - uber-raw-data-20140401-sorted.csv', 'csv', 'header');
+//console.log(uber);
  
- // uber = loadTable('https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-apr14.csv', 'csv', 'header');
-
-=======
-
-  uber = loadTable('https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-apr14.csv', 'csv', 'header');
->>>>>>> 2696ca2c746c0240a3653a2897b2a4602f81a82c
 
   var url = "https://data.cityofnewyork.us/resource/gkne-dk5s.json";
 
-  yellowcab = loadJSON(url);
+  //yellowcab = loadJSON(url);
 
 }
 
@@ -43,7 +37,7 @@ function setup() {
   createCanvas(width, length);
   background(0);
   loadData();
-  frameRate(200);
+  frameRate(10000);
   
 }
 
@@ -55,13 +49,9 @@ function draw() {
   fill(255, 255, 0, 130);
   ellipse(cablong[s], length-cablat[s], 3, 3);
   s = s+1;
-  if (s>1000){s=0;}
-
-<<<<<<< HEAD
+  if (s>uberlat.length) {s=0;} //should stop s, when it reach uberlat.length.
 
 
-=======
->>>>>>> 2696ca2c746c0240a3653a2897b2a4602f81a82c
 }
 
 	
@@ -72,28 +62,34 @@ function loadData() {
 
 var a = uber.getColumn("Lat");
 var b = uber.getColumn("Lon");
+var c = uber.getColumn("Date/Time");
 
 
 //console.log(yellowcab);
 
-for (var i = 0; i<1000; i++) { 
+for (var i = 0; i<a.length; i++) { 
 
 	var x = map(a[i], 40.65, 40.85, 0, width);
 	var y = map(b[i], -74.10, -73.90, 0, length);
-  var x1 = map(Number(yellowcab[i].pickup_latitude), 40.65, 40.85, 0, width);
-  var y1 = map(Number(yellowcab[i].pickup_longitude), -74.10, -73.90, 0, length);
+ 
+  //var x1 = map(Number(yellowcab[i].pickup_latitude), 40.65, 40.85, 0, width);
+  //var y1 = map(Number(yellowcab[i].pickup_longitude), -74.10, -73.90, 0, length);
 
   append(uberlat, x);
   append(uberlong, y);
-  append(cablat, x1);
-  append(cablong, y1);
+  //append(cablat, x1);
+  //append(cablong, y1);
 
 }
 
-<<<<<<< HEAD
-//console.log(cablat);
-=======
-console.log(cablat);
->>>>>>> 2696ca2c746c0240a3653a2897b2a4602f81a82c
+
+for (var i = 0; i<60; i++) {
+
+   var x1 = Number(c[i].substring(10, 11))+ Number(c[i].substring(13, 14));
+   append(ubertime, x1);
+}
+
+//console.log(ubertime);
+
 
 }
